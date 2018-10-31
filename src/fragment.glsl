49612cd4046ca86e8e52dcb74c462da2c23c0db8
CobaltXII <sidatt64@gmail.com>
2018-10-31 07:35:38 -0400

    Modified fragment.glsl

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -2,8 +2,8 @@ void main()
 {
 	glx_FragColor = vec4
 	(
-		mod(glx_FragCoord.x + glx_Time * 500.0f, glx_Resolution.x) / glx_Resolution.x, 
-		mod(glx_FragCoord.y + glx_Time * 500.0f, glx_Resolution.y) / glx_Resolution.y, 
+		mod(glx_FragCoord.x + glx_Time * 500.0f, glx_Resolution.x / 2.0f) / glx_Resolution.x * 2.0f, 
+		mod(glx_FragCoord.y + glx_Time * 500.0f, glx_Resolution.y / 2.0f) / glx_Resolution.y * 2.0f, 
 
 		abs(sin(glx_Time * 2.0f)), 
 
