37c6e81d4d5de30351c0b935b23ca722788710c1
erba <eric.bachard@free.fr>
2019-04-21 13:15:12 +0200

    Fix some build issues, fix resizable window, fix missin glViewport, fix warnings and remove some tabs for spaces instead

--- src/shaker.cpp
+++ src/shaker.cpp
@@ -41,15 +41,49 @@ Shaker is licensed under the GNU GPLv3. SDL2 is licensed under the zlib license.
 #include <SDL2/SDL.h>
 
 /*
+   use 1 if you want to use vsinc (around 60 fps, following display refresh),
+   else 0  (unlimited fps)
+*/
+
+#define SHAKER_VSINC 0
+
+#define DEFAULT_WIDTH  1280
+#define DEFAULT_HEIGHT  720
+
+/*
 
 For some reason, Apple has deprecated OpenGL since macOS 10.14. However, the API still functions
 perfectly, Apple has just dropped support for updating it.
 
 */
 
+
+
 #define GL_SILENCE_DEPRECATION
 
+#ifdef Linux
+#include <iostream>
+/*  mandatory */
+#define GL3_PROTOTYPES 1
+#define GL_GLEXT_PROTOTYPES
+#include <GL/glew.h>
+#include <GL/gl.h>
+
+/* adapt to your needs */
+#define MY_GL_MAJOR_VERSION  4
+#define MY_GL_MINOR_VERSION  5
+
+#else
+/*  Windows or maybe Mac OS X*/
+#define GL_MAJOR_VERSION  3
+#define GL_MINOR_VERSION  2
 #include <OpenGL/GL3.h>
+#endif
+
+
+static int gl_w = DEFAULT_WIDTH;
+static int gl_h = DEFAULT_HEIGHT;
+
 
 /*
 
@@ -106,20 +140,20 @@ Load std::string from file.
 
 std::string loadfile(std::string path)
 {
-	std::ifstream _in_file(path);
+std::ifstream _in_file(path);
 
-	if (!_in_file.is_open())
-	{
-		std::cout << "Could not load \"" << path << "\"." << std::endl;
+if (!_in_file.is_open())
+{
+std::cout << "Could not load \"" << path << "\"." << std::endl;
 
-		exit(3);
-	}
+exit(3);
+}
 
-	std::stringstream _str_buffer;
+std::stringstream _str_buffer;
 
-	_str_buffer << _in_file.rdbuf() << "\0";
+_str_buffer << _in_file.rdbuf() << "\0";
 
-	return _str_buffer.str();
+return _str_buffer.str();
 }
 
 /*
@@ -130,405 +164,371 @@ Entry point.
 
 int main(int argc, char** argv)
 {
-	/*
+    /*
+        Parse command line input.
+    */
+
+    const char* usr_fragment_path;
+
+    if (argc == 2)
+    {
+        usr_fragment_path = argv[1];
+    }
+    else if (argc == 4)
+    {
+        gl_w = atoi(argv[2]);
+        gl_h = atoi(argv[3]);
 
-	Parse command line input.
+        usr_fragment_path = argv[1];
+    }
+    else
+    {
+        std::cout << "Usage: shaker <fragment-path> [width, height]" << std::endl;
+        exit(7);
+    }
 
-	*/
+    /*
+        Initialize SDL2.
+    */
 
-	int sdl_xres;
-	int sdl_yres;
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    {
+        std::cout << "Could not initialize SDL." << std::endl;
+        exit(1);
+    }
 
-	const char* usr_fragment_path;
 
-	if (argc == 2)
-	{
-		sdl_xres = 800;
-		sdl_yres = 600;
+    SDL_Window* sdl_window = SDL_CreateWindow
+    (
+        (std::string("Shaker - ") + usr_fragment_path).c_str(),
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        gl_w, gl_h,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE
+    );
 
-		usr_fragment_path = argv[1];
-	}
-	else if (argc == 4)
-	{
-		sdl_xres = atoi(argv[2]);
-		sdl_yres = atoi(argv[3]);
+    if (sdl_window == NULL)
+    {
+        std::cout << "Could not create a SDL_Window." << std::endl;
+        exit(2);
+    }
 
-		usr_fragment_path = argv[1];
-	}
-	else
-	{
-		std::cout << "Usage: shaker <fragment-path> [width, height]" << std::endl;
+    /*
+        Request OpenGL 3.2.
+    */
 
-		exit(7);
-	}
+    int gl_success = 0;
 
-	/*
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
-	Initialize SDL2.
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, MY_GL_MAJOR_VERSION);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, MY_GL_MINOR_VERSION);
 
-	*/
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
-	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
-	{
-		std::cout << "Could not initialize SDL." << std::endl;
+    /*
+        Initialize OpenGL.
+    */
 
-		exit(1);
-	}
+    SDL_GLContext gl_context = SDL_GL_CreateContext(sdl_window);
 
-	SDL_Window* sdl_window = SDL_CreateWindow
-	(
-		(std::string("Shaker - ") + usr_fragment_path).c_str(),
+    if (gl_context == NULL)
+    {
+        std::cout << "Could not create a SDL_GLContext." << std::endl;
+        exit(2);
+    }
 
-		SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,
+#ifdef Linux
+        glewExperimental = GL_TRUE;
+        glewInit();
+#endif
+        SDL_GL_GetDrawableSize(sdl_window, &gl_w, &gl_h);
+#ifdef SHAKER_DEBUG
+        std::cout << "OpenGL version: " << glGetString(GL_VERSION) << "." << std::endl;
+        std::cout << "OpenGL drawable size: " << gl_w << ", " << gl_h << "." << std::endl;
+#endif
+        SDL_GL_SetSwapInterval(SHAKER_VSINC);
+        /*
+                Load and compile shaders.
+        */
 
-		sdl_xres,
-		sdl_yres,
+        int gl_vertex_shader = glCreateShader(GL_VERTEX_SHADER);
 
-		SDL_WINDOW_OPENGL |
+        glShaderSource(gl_vertex_shader, 1, &gl_vertex_shader_source, NULL);
 
-		SDL_WINDOW_ALLOW_HIGHDPI
-	);
+        glCompileShader(gl_vertex_shader);
 
-	if (sdl_window == NULL)
-	{
-		std::cout << "Could not create a SDL_Window." << std::endl;
+        glGetShaderiv(gl_vertex_shader, GL_COMPILE_STATUS, &gl_success);
 
-		exit(2);
-	}
+        if (!gl_success)
+        {
+            char gl_info[4096];
+            glGetShaderInfoLog(gl_vertex_shader, 4096, NULL, gl_info);
+            std::cout << "Could not compile vertex shader." << std::endl;
 
-	/*
+            std::cout << gl_info << std::endl;
 
-	Request OpenGL 3.2.
+            exit(4);
+        }
 
-	*/
+        std::string gl_fragment_shader_source = std::string(gl_fragment_shader_header) + loadfile(usr_fragment_path);
 
-	int gl_success = 0;
+        const GLchar* gl_char_fragment_shader_source = (const GLchar*)gl_fragment_shader_source.c_str();
 
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+        int gl_fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
 
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+        glShaderSource(gl_fragment_shader, 1, &gl_char_fragment_shader_source, NULL);
 
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+        glCompileShader(gl_fragment_shader);
 
-	/*
+        glGetShaderiv(gl_fragment_shader, GL_COMPILE_STATUS, &gl_success);
 
-	Initialize OpenGL.
+        if (!gl_success)
+        {
+            char gl_info[4096];
 
-	*/
+            glGetShaderInfoLog(gl_fragment_shader, 4096, NULL, gl_info);
 
-	SDL_GLContext gl_context = SDL_GL_CreateContext(sdl_window);
+            std::cout << "Could not compile fragment shader." << std::endl;
+            std::cout << gl_info << std::endl;
+            exit(5);
+        }
 
-	if (gl_context == NULL)
-	{
-		std::cout << "Could not create a SDL_GLContext." << std::endl;
+        /*
+            Link shaders.
+        */
 
-		exit(2);
-	}
+        int gl_shader_program = glCreateProgram();
 
-	int gl_w;
-	int gl_h;
+        glAttachShader(gl_shader_program, gl_vertex_shader);
 
-	SDL_GL_GetDrawableSize(sdl_window, &gl_w, &gl_h);
+        glAttachShader(gl_shader_program, gl_fragment_shader);
 
-	#ifdef SHAKER_DEBUG
+        glLinkProgram(gl_shader_program);
 
-	std::cout << "OpenGL version: " << glGetString(GL_VERSION) << "." << std::endl;
+        glGetProgramiv(gl_shader_program, GL_LINK_STATUS, &gl_success);
 
-	std::cout << "OpenGL drawable size: " << gl_w << ", " << gl_h << "." << std::endl;
+        if (!gl_success)
+        {
+            char gl_info[4096];
 
-	#endif
+            glGetProgramInfoLog(gl_shader_program, 4096, NULL, gl_info);
 
-	SDL_GL_SetSwapInterval(1);
+            std::cout << "Could not link vertex and fragment shaders." << std::endl;
+            std::cout << gl_info;
+            exit(6);
+        }
 
-	/*
+        /*
+            Delete shaders.
+        */
 
-	Load and compile shaders.
+        glDeleteShader(gl_vertex_shader);
+        glDeleteShader(gl_fragment_shader);
 
-	*/
+        /*
+            Define quadrant vertices and indices.
+        */
 
-	int gl_vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+        float gl_vertices[] =
+        {
+            0.0f + 1.0f, 0.0f + 1.0f, 0.0f + 0.0f,
+            0.0f + 1.0f, 0.0f - 1.0f, 0.0f + 0.0f,
+            0.0f - 1.0f, 0.0f - 1.0f, 0.0f + 0.0f,
+            0.0f - 1.0f, 0.0f + 1.0f, 0.0f + 0.0f,
+        };
 
-	glShaderSource(gl_vertex_shader, 1, &gl_vertex_shader_source, NULL);
+        unsigned int gl_indices[] =
+        {
+            0, 1, 3,
+            1, 2, 3
+        };
 
-	glCompileShader(gl_vertex_shader);
+        /*
+            Generate the vertex buffer object, vertex array object, and element buffer object.
+        */
 
-	glGetShaderiv(gl_vertex_shader, GL_COMPILE_STATUS, &gl_success);
+        unsigned int gl_vbo;
+        unsigned int gl_vao;
+        unsigned int gl_ebo;
 
-	if (!gl_success)
-	{
-		char gl_info[4096];
+        glGenVertexArrays(1, &gl_vao);
 
-		glGetShaderInfoLog(gl_vertex_shader, 4096, NULL, gl_info);
+        glGenBuffers(1, &gl_vbo);
+        glGenBuffers(1, &gl_ebo);
 
-		std::cout << "Could not compile vertex shader." << std::endl;
+        glBindVertexArray(gl_vao);
+
+        glBindBuffer(GL_ARRAY_BUFFER, gl_vbo);
+
+        glBufferData(GL_ARRAY_BUFFER, sizeof(gl_vertices), gl_vertices, GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gl_ebo);
 
-		std::cout << gl_info;
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(gl_indices), gl_indices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), NULL);
 
-		exit(4);
-	}
+        glEnableVertexAttribArray(0);
 
-	std::string gl_fragment_shader_source = std::string(gl_fragment_shader_header) + loadfile(usr_fragment_path);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	const GLchar* gl_char_fragment_shader_source = (const GLchar*)gl_fragment_shader_source.c_str();
+        glUseProgram(gl_shader_program);
 
-	int gl_fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+        /*
+            Enter main loop.
+        */
 
-	glShaderSource(gl_fragment_shader, 1, &gl_char_fragment_shader_source, NULL);
+#ifdef USE_SDL_MOUSE_BUTTONS
+        /* FIXME : all variables below are unused */
+        Uint32 sdl_mouse_x = 0;
+        Uint32 sdl_mouse_y = 0;
+        SDL_bool sdl_mouse_l = SDL_FALSE;
+        SDL_bool sdl_mouse_r = SDL_FALSE;
+#endif
+        Uint32 sdl_iteration = 0;
 
-	glCompileShader(gl_fragment_shader);
+        SDL_bool sdl_running = SDL_TRUE;
 
-	glGetShaderiv(gl_fragment_shader, GL_COMPILE_STATUS, &gl_success);
+        while (sdl_running == SDL_TRUE)
+        {
+            Uint32 sdl_time_start = SDL_GetTicks();
 
-	if (!gl_success)
-	{
-		char gl_info[4096];
+            /*
+                Handle events.
 
-		glGetShaderInfoLog(gl_fragment_shader, 4096, NULL, gl_info);
+            */
 
-		std::cout << "Could not compile fragment shader." << std::endl;
+            SDL_Event e;
 
-		std::cout << gl_info;
+            while (SDL_PollEvent(&e))
+            {
+                if (e.type == SDL_QUIT)
+                {
+                    sdl_running = SDL_FALSE;
+                }
+                else if (e.type == SDL_WINDOWEVENT)
+                {
+                    switch( e.window.event)
+                    {
+                        case SDL_WINDOWEVENT_RESIZED:
+                            gl_w = e.window.data1;
+                            gl_h = e.window.data2;
+#ifdef SHAKER_DEBUG
+                            std::cout << "Window resized, width = " << gl_w << " , height = " << gl_h << std::endl;
+#endif
+                        break;
 
-		exit(5);
-	}
+                        default:
+                            break;
+                    }
+                }
+#ifdef USE_SDL_MOUSE_BUTTONS
+                else if (e.type == SDL_MOUSEMOTION)
+                {
+                    sdl_mouse_x = e.motion.x;
+                    sdl_mouse_y = e.motion.y;
+                }
+                else if (e.type == SDL_MOUSEBUTTONDOWN)
+                {
+                    if (e.button.button == SDL_BUTTON_LEFT)
+                    {
+                        sdl_mouse_l = SDL_TRUE;
+                    }
+                    else if (e.button.button == SDL_BUTTON_RIGHT)
+                    {
+                        sdl_mouse_r = SDL_TRUE;
+                    }
+                }
+                else if (e.type == SDL_MOUSEBUTTONUP)
+                {
+                    if (e.button.button == SDL_BUTTON_LEFT)
+                    {
+                        sdl_mouse_l = SDL_FALSE;
+                    }
+                    else if (e.button.button == SDL_BUTTON_RIGHT)
+                    {
+                        sdl_mouse_r = SDL_FALSE;
+                    }
+                }
+#endif  /* USE_SDL_MOUSE_BUTTONS */
+                else if (e.type == SDL_KEYDOWN)
+                {
+                    SDL_Keycode sdl_key = e.key.keysym.sym;
 
-	/*
+                    if (sdl_key == SDLK_ESCAPE)
+                    {
+                        sdl_running = SDL_FALSE;
+                    }
+                }
+            }
 
-	Link shaders.
 
-	*/
+            /*
+                Set up variables.
+            */
 
-	int gl_shader_program = glCreateProgram();
+            int gl_i_resolution_location = glGetUniformLocation(gl_shader_program, "glx_Resolution");
 
-	glAttachShader(gl_shader_program, gl_vertex_shader);
+            glUniform2f(gl_i_resolution_location, (float)gl_w, (float)gl_h);
 
-	glAttachShader(gl_shader_program, gl_fragment_shader);
+            int gl_i_time_location = glGetUniformLocation(gl_shader_program, "glx_Time");
 
-	glLinkProgram(gl_shader_program);
+            glUniform1f(gl_i_time_location, (float)sdl_iteration / 60.0f);
 
-	glGetProgramiv(gl_shader_program, GL_LINK_STATUS, &gl_success);
+            /*
+                Draw quadrant.
+            */
 
-	if (!gl_success)
-	{
-		char gl_info[4096];
+            /*  clear the color buffer */
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		glGetProgramInfoLog(gl_shader_program, 4096, NULL, gl_info);
+            /* set the viewport, when e.g. the window size changed ... */
+            glViewport(0, 0, gl_w, gl_h);
 
-		std::cout << "Could not link vertex and fragment shaders." << std::endl;
+            /* draw */
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-		std::cout << gl_info;
+            /* render */
+            SDL_GL_SwapWindow(sdl_window);
 
-		exit(6);
-	}
+            /*
+                Cap framerate.
+            */
 
-	/*
+            Uint32 sdl_time_elapsed = SDL_GetTicks() - sdl_time_start;
 
-	Delete shaders.
+#ifdef SHAKER_DEBUG
+            if (sdl_iteration % 60 == 0)
+            {
+                std::cout << "Framerate: " << (1000.0 / sdl_time_elapsed) << std::endl;
+            }
+#endif
 
-	*/
+            if (sdl_time_elapsed < (1000.0 / 60.0))
+            {
+                SDL_Delay((Uint32)((Uint32)(1000.0 / 60.0) - sdl_time_elapsed));
+            }
 
-	glDeleteShader(gl_vertex_shader);
+            sdl_iteration++;
+        }
 
-	glDeleteShader(gl_fragment_shader);
+        /*
+            Clean up SDL2 and OpenGL.
+        */
 
-	/*
+        glDeleteVertexArrays(1, &gl_vao);
 
-	Define quadrant vertices and indices.
+        glDeleteBuffers(1, &gl_vbo);
+        glDeleteBuffers(1, &gl_ebo);
 
-	*/
+        SDL_GL_DeleteContext(gl_context);
 
-	float gl_vertices[] =
-	{
-		0.0f + 1.0f, 0.0f + 1.0f, 0.0f + 0.0f,
-		0.0f + 1.0f, 0.0f - 1.0f, 0.0f + 0.0f,
-		0.0f - 1.0f, 0.0f - 1.0f, 0.0f + 0.0f,
-		0.0f - 1.0f, 0.0f + 1.0f, 0.0f + 0.0f,
-	};
+        SDL_DestroyWindow(sdl_window);
 
-	unsigned int gl_indices[] =
-	{
-		0, 1, 3,
-		1, 2, 3
-	};
+        SDL_Quit();
 
-	/*
-
-	Generate the vertex buffer object, vertex array object, and element buffer object.
-
-	*/
-
-	unsigned int gl_vbo;
-	unsigned int gl_vao;
-	unsigned int gl_ebo;
-
-	glGenVertexArrays(1, &gl_vao);
-
-	glGenBuffers(1, &gl_vbo);
-	glGenBuffers(1, &gl_ebo);
-
-	glBindVertexArray(gl_vao);
-
-	glBindBuffer(GL_ARRAY_BUFFER, gl_vbo);
-
-	glBufferData(GL_ARRAY_BUFFER, sizeof(gl_vertices), gl_vertices, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gl_ebo);
-
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(gl_indices), gl_indices, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), NULL);
-
-	glEnableVertexAttribArray(0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	glUseProgram(gl_shader_program);
-
-	/*
-
-	Enter main loop.
-
-	*/
-
-	Uint32 sdl_mouse_x = 0;
-	Uint32 sdl_mouse_y = 0;
-
-	SDL_bool sdl_mouse_l = SDL_FALSE;
-	SDL_bool sdl_mouse_r = SDL_FALSE;
-
-	Uint32 sdl_iteration = 0;
-
-	SDL_bool sdl_running = SDL_TRUE;
-
-	while (sdl_running == SDL_TRUE)
-	{
-		Uint32 sdl_time_start = SDL_GetTicks();
-
-		/*
-
-		Handle events.
-
-		*/
-
-		SDL_Event e;
-
-		while (SDL_PollEvent(&e))
-		{
-			if (e.type == SDL_QUIT)
-			{
-				sdl_running = SDL_FALSE;
-			}
-			else if (e.type == SDL_MOUSEMOTION)
-			{
-				sdl_mouse_x = e.motion.x;
-				sdl_mouse_y = e.motion.y;
-			}
-			else if (e.type == SDL_MOUSEBUTTONDOWN)
-			{
-				if (e.button.button == SDL_BUTTON_LEFT)
-				{
-					sdl_mouse_l = SDL_TRUE;
-				}
-				else if (e.button.button == SDL_BUTTON_RIGHT)
-				{
-					sdl_mouse_r = SDL_TRUE;
-				}
-			}
-			else if (e.type == SDL_MOUSEBUTTONUP)
-			{
-				if (e.button.button == SDL_BUTTON_LEFT)
-				{
-					sdl_mouse_l = SDL_FALSE;
-				}
-				else if (e.button.button == SDL_BUTTON_RIGHT)
-				{
-					sdl_mouse_r = SDL_FALSE;
-				}
-			}
-			else if (e.type == SDL_KEYDOWN)
-			{
-				SDL_Keycode sdl_key = e.key.keysym.sym;
-
-				if (sdl_key == SDLK_ESCAPE)
-				{
-					sdl_running = SDL_FALSE;
-				}
-			}
-		}
-
-		/*
-
-		Set up variables.
-
-		*/
-
-		int gl_i_resolution_location = glGetUniformLocation(gl_shader_program, "glx_Resolution");
-
-		glUniform2f(gl_i_resolution_location, (float)gl_w, (float)gl_h);
-
-		int gl_i_time_location = glGetUniformLocation(gl_shader_program, "glx_Time");
-
-		glUniform1f(gl_i_time_location, (float)sdl_iteration / 60.0f);
-
-		/*
-
-		Draw quadrant.
-
-		*/
-
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-		SDL_GL_SwapWindow(sdl_window);
-
-		/*
-
-		Cap framerate.
-
-		*/
-
-		Uint32 sdl_time_elapsed = SDL_GetTicks() - sdl_time_start;
-
-		#ifdef SHAKER_DEBUG
-
-		if (sdl_iteration % 60 == 0)
-		{
-			std::cout << "Framerate: " << (1000.0 / sdl_time_elapsed) << std::endl;
-		}
-		
-		#endif
-
-		if (sdl_time_elapsed < 1000.0 / 60.0)
-		{
-			SDL_Delay((Uint32)((Uint32)(1000.0 / 60.0) - sdl_time_elapsed));
-		}
-
-		sdl_iteration++;
-	}
-
-	/*
-
-	Clean up SDL2 and OpenGL.
-
-	*/
-
-	glDeleteVertexArrays(1, &gl_vao);
-
-	glDeleteBuffers(1, &gl_vbo);
-	glDeleteBuffers(1, &gl_ebo);
-
-	SDL_GL_DeleteContext(gl_context);
-
-	SDL_DestroyWindow(sdl_window);
-
-	SDL_Quit();
-
-	/*
-
-	Exit cleanly.
-
-	*/
-
-	return 0;
+        /*
+            Exit cleanly.
+        */
+        return 0;
 }
