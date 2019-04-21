233544dae3a0811f6ec6123ba42e729c926598a5
erba <eric.bachard@free.fr>
2019-04-21 13:15:56 +0200

    Slow down the animation

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -5,7 +5,7 @@ void main()
 		mod(glx_FragCoord.x + glx_Time * 500.0f, glx_Resolution.x / 2.0f) / glx_Resolution.x * 2.0f, 
 		mod(glx_FragCoord.y + glx_Time * 500.0f, glx_Resolution.y / 2.0f) / glx_Resolution.y * 2.0f, 
 
-		abs(sin(glx_Time * 2.0f)), 
+		abs(sin(glx_Time * 0.1f)), 
 
 		1.0f
 	);
